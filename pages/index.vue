e4a781fdd45d615d3120339c09b4374bfcd0de2f
Tyler Ruff <truff@blazed.work>
2023-12-27 08:25:31 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
 	<div class="bg-gray-100 text-gray-800">
 		<div class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-5xl">
 			<h1 class="text-4xl font-bold leadi sm:text-5xl">
-                Your Gateway to Seamless Connectivity and <span class="text-red-600">Cutting-Edge Technology!</span>
+                Your Gateway to <span class="text-red-600">Cutting-Edge Technology</span>
 			</h1>
 			<p class="px-8 mt-8 mb-8 text-lg">
                 At Blazed Systems, we're not just about products; we're about solutions. 
